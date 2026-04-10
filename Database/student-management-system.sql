73ee0eec2281712b715edbbb49a8dc548a2881a4
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2026-04-10 20:25:32 +0800

    Updated database SQL backup

--- Database/student-management-system.sql
+++ Database/student-management-system.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5ubuntu0.5
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: May 30, 2022 at 07:57 PM
--- Server version: 5.7.38-0ubuntu0.18.04.1
--- PHP Version: 7.4.29
+-- Host: 127.0.0.1
+-- Generation Time: Apr 10, 2026 at 02:21 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `schoolnew1`
+-- Database: `student-management-system`
 --
 
 -- --------------------------------------------------------
@@ -30,40 +31,14 @@ CREATE TABLE `attendance` (
   `sid` int(10) NOT NULL,
   `date` date NOT NULL,
   `aid` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `attendance`
 --
 
 INSERT INTO `attendance` (`sid`, `date`, `aid`) VALUES
-(2, '2020-05-25', 3),
-(1, '2020-05-30', 4),
-(2, '2020-05-02', 5),
-(2, '1975-09-17', 6),
-(3, '2005-06-30', 7);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `attendancereport`
---
-
-CREATE TABLE `attendancereport` (
-  `aid` int(20) NOT NULL,
-  `sid` varchar(50) NOT NULL,
-  `status` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `attendancereport`
---
-
-INSERT INTO `attendancereport` (`aid`, `sid`, `status`) VALUES
-(3, 'ST1000010001', 'Absent'),
-(3, 'ST1000010002', 'Present'),
-(4, 'ST1000010001', 'Present'),
-(4, 'ST1000010002', 'Present');
+(1, '2026-04-08', 1);
 
 -- --------------------------------------------------------
 
@@ -76,16 +51,22 @@ CREATE TABLE `classroom` (
   `title` varchar(50) NOT NULL,
   `location` varchar(50) NOT NULL,
   `capacity` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `classroom`
 --
 
 INSERT INTO `classroom` (`hno`, `title`, `location`, `capacity`) VALUES
+('10-A', 'Pioneer', 'Block-G', 52),
+('10-B', 'Summit', 'Block-H', 58),
 ('4-B', 'Nilwala', 'Block-D', 50),
-('Aute in irure autem ', 'Aut magnam fugiat r', 'Aliquam minim numqua', 12),
-('Sunt cillum qui iust', 'Eum hic ipsum ration', 'Ut nostrum laborum ', 48);
+('7-A', 'Sunshine', 'Block-A', 45),
+('7-B', 'Evergreen', 'Block-B', 50),
+('8-A', 'Horizon', 'Block-C', 55),
+('8-B', 'Galaxy', 'Block-D', 60),
+('9-A', 'Starlight', 'Block-E', 40),
+('9-B', 'Blue Ocean', 'Block-F', 48);
 
 -- --------------------------------------------------------
 
@@ -101,15 +82,14 @@ CREATE TABLE `exam` (
   `date` date NOT NULL,
   `stime` time NOT NULL,
   `etime` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `exam`
 --
 
 INSERT INTO `exam` (`id`, `subject`, `teacher`, `classroom`, `date`, `stime`, `etime`) VALUES
-(1, 'SCM4251', 'TC1000020000', '4-B', '2020-05-26', '11:45:00', '12:45:00'),
-(2, '', 'Andrew', '4-B', '2022-05-20', '00:00:00', '00:00:00');
+(1, 'SCM101', 'TC1001', '4-B', '2026-04-15', '09:00:00', '10:00:00');
 
 -- --------------------------------------------------------
 
@@ -122,17 +102,14 @@ CREATE TABLE `examresult` (
   `student` varchar(50) NOT NULL,
   `marks` int(10) NOT NULL,
   `grade` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `examresult`
 --
 
 INSERT INTO `examresult` (`exam`, `student`, `marks`, `grade`) VALUES
-(1, 'Brittany', 44, 'C-'),
-(1, 'ST1000010001', 55, 'C+'),
-(1, 'ST1000010002', 77, 'A+'),
-(2, 'Kiayada', 91, 'C+');
+(1, 'STU1001', 95, 'A');
 
 -- --------------------------------------------------------
 
@@ -145,15 +122,14 @@ CREATE TABLE `notice` (
   `notice` varchar(1500) NOT NULL,
   `odience` varchar(100) NOT NULL,
   `date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `notice`
 --
 
 INSERT INTO `notice` (`id`, `notice`, `odience`, `date`) VALUES
-(5, 'Ea cum labore illo n', 'Parent', '2022-05-30 10:52:24'),
-(6, 'Dolores suscipit qui', 'Select Odience', '2022-05-30 10:52:27');
+(1, 'School closed on Friday', 'All', '2026-04-10 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -171,16 +147,20 @@ CREATE TABLE `parent` (
   `gender` varchar(25) NOT NULL,
   `nic` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `parent`
 --
 
 INSERT INTO `parent` (`pid`, `fname`, `lname`, `contact`, `job`, `address`, `gender`, `nic`, `email`) VALUES
-(1, 'Kelly', 'Kelly', '0785566022555', 'Engineer', 'Colombo Road\r\nPilimathalawa.', 'Male', '7855485552V', 'parent@gmail.com'),
-(2, 'Natasha', 'Belle', 'Rebecca', 'Alvin', 'Laborum Iure ea nos', 'Female', 'Aladdin', 'qiwip@mailinator.com'),
-(3, 'Taylor', 'Swift', 'Roary', 'Price', 'Laborum maiores vel ', 'Male', 'Merrill', 'dabecapywa@mailinator.com');
+(1, 'Mary', 'Doe', '09112223344', 'Engineer', '123 Main St', 'Female', '123456789V', 'parent@gmail.com'),
+(7, 'Mary', 'Doe', '09112223344', 'Engineer', '123 Main St', 'Female', '123456789V', 'paren@gmail.com'),
+(8, 'John', 'Smith', '09998887766', 'Teacher', '456 Oak Ave', 'Male', '987654321V', 'johnparent@gmail.com'),
+(9, 'Anna', 'Cruz', '09223334455', 'Nurse', '789 Pine Rd', 'Female', '112233445V', 'anna@gmail.com'),
+(10, 'Mark', 'Reyes', '09112220011', 'Driver', '321 Elm St', 'Male', '556677889V', 'mark@gmail.com'),
+(11, 'Liza', 'Santos', '09334455667', 'Accountant', '654 Maple St', 'Female', '998877665V', 'liza@gmail.com'),
+(12, 'Rowena', 'Embodo', '123456723456', 'Teacher', 'Bobon A', 'Female', '676568765V', 'albuenmonaliza@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -196,21 +176,14 @@ CREATE TABLE `schedule` (
   `stime` time NOT NULL,
   `class` varchar(50) NOT NULL,
   `etime` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `schedule`
 --
 
 INSERT INTO `schedule` (`id`, `subject`, `teacher`, `day`, `stime`, `class`, `etime`) VALUES
-(1, 'SCM4251', 'TC1000020000', 'Wendsday', '04:15:00', '4-B', '04:15:00'),
-(2, 'SCM4251', 'TC1000020000', 'Thursday', '05:30:00', '4-B', '07:45:00'),
-(3, 'Voluptatem Consequa', 'Andrew', 'Monday', '01:21:23', '4-B', '00:01:23'),
-(4, 'Quas laudantium nem', 'TC1000020000', 'Thursday', '12:31:23', 'Aute in irure autem ', '00:01:23'),
-(5, 'Sequi fugiat molliti', 'Andrew', 'Sunday', '00:00:00', 'Select Class Room', '00:00:00'),
-(6, '', 'TC1000020000', 'Saturday', '00:00:00', 'Aute in irure autem ', '00:00:00'),
-(7, 'Select Subject', 'Andrew', 'Monday', '00:00:00', 'Select Class Room', '00:00:00'),
-(8, 'Quaerat illum nesci', 'Andrew', 'Wendsday', '00:00:00', 'Aute in irure autem ', '00:00:00');
+(1, 'SCM101', 'TC1001', 'Monday', '09:00:00', '4-B', '10:00:00');
 
 -- --------------------------------------------------------
 
@@ -228,24 +201,25 @@ CREATE TABLE `student` (
   `gender` varchar(10) NOT NULL,
   `classroom` varchar(25) NOT NULL,
   `email` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `student`
 --
 
 INSERT INTO `student` (`sid`, `fname`, `lname`, `bday`, `address`, `parent`, `gender`, `classroom`, `email`) VALUES
-('Bethany', 'Walker', 'Nathaniel', '1971-02-23', 'Velit accusamus rer', 0, 'Male', 'Select Class Room', 'behof@mailinator.com'),
-('Brittany', 'Noah', 'Odessa', '2017-08-22', 'Expedita nobis et al', 0, 'Male', 'Select Class Room', 'hocy@mailinator.com'),
-('Brody', 'Kendall', 'Isabelle', '1989-01-11', 'Nostrud dolor est ve', 1, 'Female', '4-B', 'vabylem@mailinator.com'),
-('Illana', 'Mufutau', 'Walker', '2016-04-06', 'In voluptates rem no', 0, 'Male', '4-B', 'sibokuzi@mailinator.com'),
-('Kiayada', 'Robin', 'Ariel', '1997-05-31', 'Magna architecto sol', 0, 'Female', '4-B', 'qofydynif@mailinator.com'),
-('Lilah', 'Miranda', 'Xavier', '2010-03-12', 'Proident vel perspi', 1, 'Female', '4-B', 'xohunelocy@mailinator.com'),
-('Rafael', 'Montana', 'Molly', '2017-12-29', 'Cumque voluptatem O', 1, 'Male', 'Select Class Room', 'tiqohababo@mailinator.com'),
-('ST1000010001', 'Kasun1', 'Chamara', '2022-05-19', 'Colombo Road \r\nKandy', 1, 'Female', '4-B', 'student@gmail.com'),
-('ST1000010002', 'Dasun4', 'Shanuka', '2020-05-31', 'Ampara Road \r\nUhana', 1, 'Male', '4-B', 'stu1@stu1.stu1'),
-('STU1000040000', 'Dilip', 'Silva', '2022-05-19', 'asasas', 1, 'Male', '4-B', 'dil@dil.dil'),
-('STU100004005', 'Hashini', 'Asiri', '2020-05-27', 'asassas', 1, 'Female', 'Select Class Room', 'h@h.h');
+('STU1001', 'John', 'Doe', '2010-01-01', '123 Main St', 1, 'Male', '4-B', 'john@student.com'),
+('STU1002', 'Monaliza', 'Albuen', '2005-01-01', 'Hindag an SBSL', 8, 'Female', '4-B', 'monaliza@student.com'),
+('STU1003', 'Via', 'Embodo', '2005-01-01', 'Bobon', 10, 'Female', '9-B', 'via@student.com'),
+('STU1004', 'Lester', 'Emuslan', '2005-01-01', 'N/A', 0, 'Male', '4-B', 'lester@student.com'),
+('STU1005', 'Armark', 'Onita', '2005-01-01', 'N/A', 0, 'Male', '4-B', 'armark@student.com'),
+('STU1006', 'James', 'Esma', '2005-01-01', 'N/A', 0, 'Male', '4-B', 'james@student.com'),
+('STU1007', 'Junryl', 'Puzon', '2005-01-01', 'N/A', 0, 'Male', '4-B', 'junryl@student.com'),
+('STU1008', 'Elmir', 'Gonzales', '2005-01-01', 'catmon', 10, 'Male', '4-B', 'elmir@student.com'),
+('STU1009', 'Nico', 'Telen', '2005-01-01', 'N/A', 0, 'Male', '4-B', 'nico@student.com'),
+('STU1010', 'John Paul', 'Sibay', '2005-01-01', 'N/A', 0, 'Male', '4-B', 'johnpaul@student.com'),
+('STU1011', 'Athia', 'Alvarez', '2026-04-06', 'korea', 7, 'Female', '9-A', 'a@gmail.com'),
+('STU1012', 'Lady ', 'Albuen', '2026-03-29', 'Hindag an', 11, 'Female', '9-A', 'albuenmonaliza@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -257,25 +231,17 @@ CREATE TABLE `subject` (
   `sid` varchar(50) NOT NULL,
   `title` varchar(200) NOT NULL,
   `description` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `subject`
 --
 
 INSERT INTO `subject` (`sid`, `title`, `description`) VALUES
-('Aute est nulla volup', 'Ipsa in voluptas vo', 'Saepe officia culpa '),
-('Autem at ea sint co', 'Est recusandae Quos', 'Aut illum porro id '),
-('Et pariatur Dolores', 'Animi amet dolor f', 'Et atque et pariatur'),
-('Lorem sipsum', 'Loadfushn gulg', 'Lores informr'),
-('Odio quasi enim nost', 'Quia ut libero dolor', 'Necessitatibus non n'),
-('Praesentium quo et a', 'Dicta error est sunt', 'Est eaque ea placea'),
-('Quaerat illum nesci', 'Velit voluptatem N', 'Alias quis quam elig'),
-('Quas laudantium nem', 'Aspernatur Nam sapie', 'Ea animi culpa ill'),
-('SCM4251', 'Science and Technology', 'Chemistry Basics\r\n'),
-('Sequi fugiat molliti', 'Quas debitis volupta444', 'Ex reiciendis omnis '),
-('Vero aperiam facere ', 'Animi est sed aliq', 'Atque quidem explica'),
-('Voluptatem Consequa', 'Dignissimos in eos ', 'Excepturi deserunt i');
+('IT101', 'System integration', 'qwdxxddd'),
+('It308', 'Machine Learnin', 'assjksksks'),
+('SCM101', 'Mathematics', 'Basic Math'),
+('SCM102', 'Science', 'General Science');
 
 -- --------------------------------------------------------
 
@@ -293,16 +259,14 @@ CREATE TABLE `teacher` (
   `skill` varchar(500) NOT NULL,
   `gender` varchar(25) NOT NULL,
   `email` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `teacher`
 --
 
 INSERT INTO `teacher` (`tid`, `fname`, `lname`, `address`, `contact`, `bday`, `skill`, `gender`, `email`) VALUES
-('Andrew', 'Lawrence45', 'Odette', 'Ipsa et distinctio', 'Eve123123', '2022-05-19', 'Ut voluptatem dolor', 'Female', 'hina@mailinator.com'),
-('Ramona', 'Raphael', 'Seth', 'Tempor sit doloribu', 'Roary', '1978-09-27', 'Rerum dolor et nulla', 'Male', 'gojewili@mailinator.com'),
-('TC1000020000', 'Nimal ', 'Soyza555', 'Kandy Road\r\nNittambuwa', '0339988554', '2022-05-21', 'Science\r\nMathematics\r\nHistory', 'Male', 'tea@tea.tea');
+('TC1001', 'Alice', 'Smith', '456 School Rd', '09123456789', '1980-05-20', 'Math', 'Female', 'alice@school.com');
 
 -- --------------------------------------------------------
 
@@ -314,16 +278,17 @@ CREATE TABLE `user` (
   `role` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`role`, `email`, `password`) VALUES
-('Parent', 'parent@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
-('Student', 'student@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
-('Teacher', 'teacher@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055');
+('Admin', 'admin@gmail.com', '0192023a7bbd73250516f069df18b500'),
+('Parent', 'parent@gmail.com', '460de495837215710a29db07c94180ca'),
+('Student', 'student@gmail.com', 'ad6a280417a0f533d8b670c61667e1a0'),
+('Teacher', 'teacher@gmail.com', 'a426dcf72ba25d046591f81a5495eab7');
 
 --
 -- Indexes for dumped tables
@@ -334,12 +299,6 @@ INSERT INTO `user` (`role`, `email`, `password`) VALUES
 --
 ALTER TABLE `attendance`
   ADD PRIMARY KEY (`aid`);
-
---
--- Indexes for table `attendancereport`
---
-ALTER TABLE `attendancereport`
-  ADD PRIMARY KEY (`aid`,`sid`);
 
 --
 -- Indexes for table `classroom`
@@ -412,27 +371,33 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `exam`
 --
 ALTER TABLE `exam`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `notice`
 --
 ALTER TABLE `notice`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `parent`
 --
 ALTER TABLE `parent`
-  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `schedule`
 --
 ALTER TABLE `schedule`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
